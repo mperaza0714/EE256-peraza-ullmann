fed1ff064f6261ec5f93b2b5bf7d5d6ebaf3f5ba
mperaza0714 <mperaza0714@gmail.com>
2023-12-07 00:50:54 -0800

    Update KnockBox.py

--- Code/KnockBox.py
+++ Code/KnockBox.py
@@ -6,9 +6,14 @@ import busio
 import audioio
 import digitalio
 import storage
-import adafruit_sdcard  # TODO add this library
+from random import randint
+import adafruit_sdcard  # TODO add libraries
 from analogio import AnalogIn
 from adafruit_debouncer import Debouncer
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+import adafruit_requests as requests
+from adafruit_io.adafruit_io import IO_HTTP, AdafruitIO_RequestError
 
 ## Set up the pins
 
@@ -54,6 +59,57 @@ playState = 2
 knockState = 3
 # initial state
 state = idleState
+
+# WIFI Setup
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+# If you have an externally connected ESP32:
+esp32_cs = DigitalInOut(board.D10)
+esp32_ready = DigitalInOut(board.D4)
+esp32_reset = DigitalInOut(board.D6)
+
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+print("Connecting to AP...")
+while not esp.is_connected:
+    try:
+        esp.connect_AP(secrets["ssid"], secrets["password"])
+    except RuntimeError as e:
+        print("could not connect to AP, retrying: ", e)
+        continue
+print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
+
+socket.set_interface(esp)
+requests.set_socket(socket, esp)
+
+aio_username = secrets["aio_username"]
+aio_key = secrets["aio_key"]
+
+# Initialize an Adafruit IO HTTP API object
+io = IO_HTTP(aio_username, aio_key, requests)
+
+try:
+    # Get the 'temperature' feed from Adafruit IO
+    temperature_feed = io.get_feed("temperature")
+except AdafruitIO_RequestError:
+    # If no 'temperature' feed exists, create one
+    temperature_feed = io.create_new_feed("temperature")
+
+# Send random integer values to the feed
+random_value = randint(0, 50)
+print("Sending {0} to temperature feed...".format(random_value))
+io.send_data(temperature_feed["key"], random_value)
+print("Data sent!")
+
+# Retrieve data value from the feed
+print("Retrieving data from temperature feed...")
+received_data = io.receive_data(temperature_feed["key"])
+print("Data from temperature feed: ", received_data["value"])
 
 
 while True:
